e958f6d07133e20b9ab589531931d3226073f38a
codeyu <codeyu@gmail.com>
2016-10-15 13:53:51 +0800

    LinkedList

---
+++ DataStructures/App.py
@@ -0,0 +1,14 @@
+import LinkedList;
+
+linkedList = LinkedList.LinkedList();
+linkedList.insertEnd(12);
+linkedList.insertEnd(122);
+linkedList.insertEnd(3);
+linkedList.insertEnd(31);
+linkedList.insertStart(100);
+
+linkedList.traverseList();
+
+linkedList.remove(3);
+
+linkedList.traverseList();
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # PythonAlgorithms
 Algorithms and Data Structures in Python
+
+![time complexities](http://codeyu.qiniudn.com/time%20complexities.png)
+
